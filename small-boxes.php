dc1bc875bde00e3bab5ad3ca81c603d2df4784ec
Thorbjörn Sundragon <puggan@spiro.se>
2018-08-30 15:06:02 +0200

    Version 1.0.1

--- small-boxes.php
+++ small-boxes.php
@@ -3,12 +3,12 @@
 	/**
 	 * @package Small Boxes
 	 * @author Puggan Sundragon <puggan@spiro.se>
-	 * @version 1.0.0
+	 * @version 1.0.1
 	 */
 	/*
 		Plugin Name: Small Boxes
 		Description: Add a simple content-type, for listing a category of them anywhere
-		Version: 1.0.0
+		Version: 1.0.1
 		Author: Puggan Sundragon <puggan@spiro.se>
 		Author URI: https://spiro.se
 		Text Domain: small_boxes
